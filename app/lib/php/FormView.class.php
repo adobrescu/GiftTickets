25d6f6257cfd46a0910af486aba8c19df09fe864
Adi Dobrescu <adobrescu@insolitservices.ro>
2016-06-11 16:41:19 +0300

    Adds simple method to FormView to display an HTML SELECT using an array keys/values

--- app/lib/php/FormView.class.php
+++ app/lib/php/FormView.class.php
@@ -6,4 +6,21 @@ include_once(__DIR__.'/View.class.php');
 
 class FormView extends View
 {
+	public function showArraySelect($arr, $nameAndId, $selectedOption, $optionNone='None', $multiple=false, $prefix='', $concatText='', $concatValue='')
+	{
+		$html='<select name="'.$nameAndId.'" id="'.$nameAndId.'"'.($multiple?' multiple':'').'>';
+		
+		if($optionNone)
+		{
+			$html.='<option value=""'.($selectedOption==''?' selected':'').'>'.$optionNone.'</option>';
+		}
+		foreach($arr as $value=>$text)
+		{
+			$html .= '<option value="'.$value.($concatValue?$concatValue.$text:'').'"'.($value==$selectedOption?' selected':'').'>'.$prefix.($concatText?$value.$concatText.' ':'').$text.'</option>';
+		}
+		
+		$html .= '</select>';
+		
+		return $html;
+	}
 }
