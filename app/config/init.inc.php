8043f19d98b9540a37688b0679123e25177649fb
Adi Dobrescu <adobrescu@insolitservices.ro>
2016-06-11 16:56:25 +0300

    Adds dummy data arrays for amounts/costs and themes

--- app/config/init.inc.php
+++ app/config/init.inc.php
@@ -3,5 +3,14 @@
 	//config settings (db, upload dir etc, libs dir, html templates dir)
 
 	define('HTML_TEMPLATE_DIR', __DIR__.'/../lib/html-templates');
+	
+	$_amounts=array(
+		'20.00' => '22.75',
+		'25.00' => '24.00'
+	);
+	$_themes=array(
+		'Birthday',
+		'Celebration'
+	);
 
 	
