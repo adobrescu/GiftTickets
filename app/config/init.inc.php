186d5f0dd41d54bfabe3d4847b6fe792dc0a23cb
Adi Dobrescu <adobrescu@insolitservices.ro>
2016-06-11 19:03:13 +0300

    Adds div around main tag

--- app/config/init.inc.php
+++ app/config/init.inc.php
@@ -4,15 +4,22 @@
 
 	define('HTML_TEMPLATE_DIR', __DIR__.'/../lib/html-templates');
 	
+	//db settings
+	
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASSWORD', '');
+	define('DB_NAME', 'gift_certificates');
+	
 	$_amounts=array(
 		'20.00' => '22.75',
-		'25.00' => '24.00'
+		'25.00' => '26.00'
 	);
 	$_themes=array(
 		'Birthday' => 'Birthday',
 		'Celebration' => 'Celebration'
 	);
 	
-	$_db=new Mysqli('localhost', 'root', '', 'gift_certificates');
+	$_db=new Mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 	
