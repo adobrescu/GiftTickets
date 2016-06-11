f6c20745e310a67cbf05982f62754ca05264dff8
Adi Dobrescu <adobrescu@insolitservices.ro>
2016-06-11 11:32:20 +0300

    Adds dummy FormView and GiftCertificateView classes

---
+++ app/lib/php/FormView.class.php
@@ -0,0 +1,9 @@
+<?php
+
+//Dummy empty class. It just impersonate a real framework "FormView" class from which this app classes inherits.
+
+include_once(__DIR__.'/View.class.php');
+
+class FormView extends View
+{
+}
---
+++ app/lib/php/GiftCertificateView.class.php
@@ -0,0 +1,18 @@
+<?php
+
+//App class inheriting from framerowk's class FormView
+
+include_once(__DIR__.'/FormView.class.php');
+
+class GiftCertificateView extends FormView
+{
+	protected $templateFilename;
+	public function __construct($templateFilename)
+	{
+		$this->templateFilename=$templateFilename;
+	}
+	public function show()
+	{
+		include($this->templateFilename);
+	}
+}
